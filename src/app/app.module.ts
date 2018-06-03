fbf459c59c40594601b5bb5542571a13038d32b4
jboullion <jboullion83@gmail.com>
2018-06-02 20:53:25 -0500

    Angular Recipe Book Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './shop/list/list.component';
+import { EditComponent } from './shop/edit/edit.component';
+import { IngredientComponent } from './shop/ingredient/ingredient.component';
+import { ItemComponent } from './recipe/item/item.component';
+import { DetailComponent } from './recipe/detail/detail.component';
+import { CommentComponent } from './recipe/comment/comment.component';
+import { RecipeComponent } from './recipe/recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListComponent,
+    EditComponent,
+    IngredientComponent,
+    ItemComponent,
+    DetailComponent,
+    CommentComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule

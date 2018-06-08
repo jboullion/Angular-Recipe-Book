c17a2754e6e9a26f0ba60d0dff82d911463afd8f
jboullion <jboullion83@gmail.com>
2018-06-07 22:09:14 -0500

    Shared Ingredient model and shopping list basic setup

--- src/app/shopping/shopping-list/shopping-list.component.ts
+++ src/app/shopping/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Meat', 4),
+    new Ingredient('Rebull', 12)
+  ];
 
-  constructor() { }
+  constructor() {
+    console.log(this.ingredients);
+  }
 
   ngOnInit() {
   }
